d7efffe82feeb9555864293d1fd4c1ff074a3bc0
dwenndson <diego.wenndson@gmail.com>
2020-02-21 13:12:39 -0300

    Create interfaces and repositorys of the Enfermeiros

---
+++ BackEnd-Selecao/Services/IGenericsServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd_Selecao.Services
+{
+    interface IGenericsServices<T>
+    {
+        List<T> GetAll();
+
+        T Save();
+    }
+}
